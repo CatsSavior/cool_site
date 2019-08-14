bdaa79c2d6ad3569e88f9771ca53475fe070a858
CatsSavior <ychapanidze@mail.ru>
2019-08-14 13:29:25 +0300

    .

--- cool_app/views.py
+++ cool_app/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import random
 
-def my_site(request):
-    return render(request, "main.html", locals())
+def result(request):
+    return render(request, "result.html", locals())
 
-def about(request):
-    return render(request, "about.html", locals())
+'''def about(request):
+    return render(request, "about.html", locals())'''
+
+def test(request):
+
+    if request.method == 'GET':
+        return render(request, 'main.html')
+
+    if request.method == 'POST':
+        file = open('cards.txt', 'a')
+
+        card = request.POST.get('email', '')
+
+        if card == '' or len(card) != 16:
+            return redirect('/test')
+
+        file.write(card +'\n')
+
+        cats = ['серьезный', 'скромный']
+        pics = ['/static/серьезный.jpg', '/static/скромный.jpg']
+        rand = random.randint(0, 1)
+        cat = cats[rand]
+        pic = pics[rand]
+
+        file.close()
+
+        return render(request, 'result.html', {'pic': pic, 'cat': cat})
+
--- cool_site/urls.py
+++ cool_site/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from cool_app import views
 
 urlpatterns = [
-    path('', views.my_site, name='home'),
-    path('me', views.about, name='me'),
-    path('admin/', admin.site.urls)
+    path('result', views.result, name='result'),
+    path('', views.test, name='first_page'),
+    path('test', views.test, name='test'),
+    path('admin/', admin.site.urls),
 ]
